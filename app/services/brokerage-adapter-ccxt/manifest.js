92c02eeb41341960efae16f37730155b95d48b2b
Konstantin A. Zolotukhin <masted@gmail.com>
2025-08-31 11:58:19 +0300

    lazy-load ccxt adapter

--- app/services/brokerage-adapter-ccxt/manifest.js
+++ app/services/brokerage-adapter-ccxt/manifest.js
@@ -1,8 +1,10 @@
 const brokerageAdapters = require('../brokerage/brokerageAdapters');
-const { CCXTExecutionAdapter } = require('./comps/ccxt');
 
 function initService() {
-  brokerageAdapters.ccxt = (cfg = {}) => new CCXTExecutionAdapter(cfg);
+  brokerageAdapters.ccxt = (cfg = {}) => {
+    const { CCXTExecutionAdapter } = require('./comps/ccxt');
+    return new CCXTExecutionAdapter(cfg);
+  };
 }
 
 module.exports = { initService };
