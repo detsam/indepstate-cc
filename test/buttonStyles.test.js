02a75565e95ace3881c68783288ecc1bf2875cd8
Konstantin A. Zolotukhin <masted@gmail.com>
2025-09-01 19:08:12 +0300

    test: remove strategyValues test

--- test/buttonStyles.test.js
+++ test/buttonStyles.test.js
@@ -45,4 +45,4 @@ async function run() {
   console.log('buttonStyles test passed');
 }
 
-run().catch(err => { console.error(err); process.exit(1); });
+run().then(() => process.exit(0)).catch(err => { console.error(err); process.exit(1); });
